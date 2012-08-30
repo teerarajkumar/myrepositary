79233e0d4ab4de6c1b6b3f301de4033058025221
rajkumar <teerarajkumar@gmail.com>
2012-08-30 14:46:58 +0530

    mytwit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+  	@user=User.new
+  end
+
+  def create
+	  @user = User.new(:email=>params[:user][:email])
+	  @user.password=params[:user][:password]
+	  @user.password_confirmation=params[:user][:password_confirmation]
+	  if @user.save
+	    redirect_to root_url, :notice => "Signed up!"
+	  else
+	    render "new"
+	  end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytwit::Application.config.secret_token = '05f652f2e1e538d06d72c76d54fb81b898c0ba3b5f88e20c30cfd82183288cc506ed79deb4c14364c1757a2d632b1b7e9e83495eb90417f1e07a8af5a24b3582'
